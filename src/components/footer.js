1add87e2be007548772da0d4a36f967f3dc4a980
Will Johnson <wy.johnson96@gmail.com>
2021-03-08 14:16:59 -0600

    Footer adjustments.

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,14 +5,14 @@ export default function Footer() {
         <div className="footer">
             <div className="container">
                 <div className="row">
-                    <div className="col-sm-6">
+                    <div className="col-sm-5">
                         <MainNav></MainNav>
                     </div>
                     <div className="col-sm-5">
                         [ social media icons ]
                     </div>
-                    <div className="col-sm-1">
-                        <small>&copy; 2021</small>
+                    <div className="col-sm-2">
+                        &copy; 2021
                     </div>
                 </div>
             </div>
